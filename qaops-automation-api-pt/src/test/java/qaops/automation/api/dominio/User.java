3288647d68cefdd016fa178a34b13266d71aee2b
Sergio Freitas <sergio.freitas.p@gmail.com>
2021-03-05 16:39:47 -0300

    Adicionando baseURI e basePath

---
+++ qaops-automation-api-pt/src/test/java/qaops/automation/api/dominio/User.java
@@ -0,0 +1,29 @@
+package qaops.automation.api.dominio;
+
+public class User {
+	private String name;
+	private String job;
+	
+	public User() {}
+	
+	public User(String name, String job) {
+		super();
+		this.name = name;
+		this.job = job;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+
+	public String getJob() {
+		return job;
+	}
+
+	
+	
+	
+	
+	
+}
